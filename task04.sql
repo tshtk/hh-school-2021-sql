30380566b529c34eb06a8870b05b8bb1573ac6df
Anton Shtyk <th@tshtk.ru>
2022-01-27 18:11:56 +0300

    refactor: refactor task04 solution

--- task04.sql
+++ task04.sql
@@ -1,23 +1,19 @@
-WITH test_data( created,
-                resume_id
-                ) AS (
-SELECT date_trunc('month', created),
-       resume_id
-FROM resume)
-SELECT count(resume_id), Extract(MONTH from created) AS mm, Extract(YEAR from created) AS yyyy
-FROM test_data
-GROUP BY created
-ORDER BY count(*) DESC
-LIMIT 1;
+--Получение месяца с наибольшим количеством вакансий и месяца с наибольшим количеством резюме
 
-WITH test_data( created,
-                vacancy_id
-                ) AS (
-SELECT date_trunc('month', created),
-       vacancy_id
-FROM vacancy)
-SELECT count(vacancy_id), Extract(MONTH from created) AS mm, Extract(YEAR from created) AS yyyy
-FROM test_data
-GROUP BY created
-ORDER BY count(*) DESC
-LIMIT 1;
+WITH max_resume_created(month, count) AS (
+  SELECT to_char(created, 'YYYY-MM'),
+         COUNT(resume_id)
+  FROM resume
+  GROUP BY to_char(created, 'YYYY-MM')
+  ORDER BY count(*) DESC
+  LIMIT 1),
+     max_vacancy_created(month, count) AS (
+  SELECT to_char(created, 'YYYY-MM'),
+         COUNT(vacancy_id)
+  FROM vacancy
+  GROUP BY to_char(created, 'YYYY-MM')
+  ORDER BY count(*) DESC
+  LIMIT 1)
+SELECT *
+FROM max_vacancy_created
+JOIN max_resume_created ON TRUE;
