b23fb62a1064e1a892d56443943d7ceac8ce94dd
Anton Shtyk <th@tshtk.ru>
2022-01-27 19:00:01 +0300

    refactor: refactor task05 solution

--- task05.sql
+++ task05.sql
@@ -1,13 +1,10 @@
 --Получение id и title вакансий, которые собрали больше 5 откликов в первую неделю после публикации
 --Также выполняется сортировка вакансий по количеству откликов в порядку убывания и выводится количество откликов
 
-WITH first_weekly_responses (vacancy_id, vacancy_title, response_created, vacancy_created) AS (
-  SELECT r.vacancy_id, v.vacancy_title, r.created, v.created
-  FROM response AS r
-  INNER JOIN vacancy AS v ON v.vacancy_id = r.vacancy_id
-  WHERE r.created - v.created <= 7)
-SELECT vacancy_id, vacancy_title, COUNT(vacancy_id) AS responses
-FROM first_weekly_responses
-GROUP BY vacancy_id, vacancy_title
-HAVING COUNT(vacancy_id)>5
+SELECT v.vacancy_id, v.vacancy_title, COUNT(r.vacancy_id) AS responses
+FROM response AS r
+INNER JOIN vacancy AS v ON v.vacancy_id = r.vacancy_id
+WHERE r.created - v.created <= 7
+GROUP BY v.vacancy_id, v.vacancy_title
+HAVING COUNT(*)>5
 ORDER BY COUNT(*) DESC;
