1f05f61d636343c2a2d5eaeff0398371c6b947a8
Anton Shtyk <th@tshtk.ru>
2022-01-27 19:25:27 +0300

    chore: fix typos

--- task06.sql
+++ task06.sql
@@ -9,6 +9,6 @@ CREATE INDEX resume_area_specialization_index ON resume(area_id, specialization_
 -- Соответствующий индекс для ускорения такого поиска
 CREATE INDEX vacancy_area_specialization_compensation_index ON vacancy (area_id, specialization_id, compensation_from, compensation_to);
 
--- Для работодателей частым вариантом поиска резюме среди откликов на конкретную вакансию
+-- Для работодателей частым вариантом поиска резюме будет поиск среди откликов на конкретную вакансию
 -- Соответствующий индекс для ускорения такого поиска
 CREATE INDEX response_vacancy_index ON response USING HASH (vacancy_id);
