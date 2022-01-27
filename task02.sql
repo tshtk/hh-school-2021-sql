00eb7b4ff89fbc5f52ee5a75ebe2d630375c4463
Anton Shtyk <th@tshtk.ru>
2022-01-27 15:38:48 +0300

    chore: add comments

--- task02.sql
+++ task02.sql
@@ -1,15 +1,15 @@
-WITH test_data( id,
-                title) AS (
-    SELECT
-           generate_series(1, 50) AS id,
-           md5(random()::text) AS title
+-- Заполнение 4 таблиц базы данных тестовыми данными
+
+WITH specialization_test_data(id, title) AS (
+  SELECT  generate_series(1, 50) AS id,
+          md5(random()::text) AS title
 )
 INSERT INTO specialization(specialization_id, specialization_title)
 SELECT id, title
-FROM test_data;
+FROM specialization_test_data;
 
 
-WITH test_data( vacancy_id,
+WITH vacancy_test_data( vacancy_id,
                 created,
                 employer_id,
                 vacancy_title,
@@ -20,19 +20,20 @@ WITH test_data( vacancy_id,
                 compensation_from,
                 compensation_interval,
                 compensation_gross) AS (
-    SELECT generate_series(1, 10000)                                AS vacancy_id,
-           now() - ('1 day'::interval * (random()*180::integer))    AS created,
-           random() * 500::integer                                  AS employer_id,
-           md5(random()::text)                                      AS vacancy_title,
-           1 + random() * 84::integer                               AS area_id,
-           1 + random() * 49::integer                               AS specialization_id,
-           1 + random() * 99::integer                               AS industry_id,
-           md5(random()::text)                                      AS vacancy_description,
-           round((random() * (80000 - 15000) + 15000)::integer, -3) AS compensation_from,
-           round((random() * (20000 - 5000) + 5000)::integer, -3)   AS compensation_interval,
-           false
+  SELECT generate_series(1, 10000)                                AS vacancy_id,
+         now() - ('1 day'::interval * (random()*180::integer))    AS created,
+         random() * 500::integer                                  AS employer_id,
+         md5(random()::text)                                      AS vacancy_title,
+         1 + random() * 84::integer                               AS area_id,
+         1 + random() * 49::integer                               AS specialization_id,
+         1 + random() * 99::integer                               AS industry_id,
+         md5(random()::text)                                      AS vacancy_description,
+         round((random() * (80000 - 15000) + 15000)::integer, -3) AS compensation_from,
+         round((random() * (20000 - 5000) + 5000)::integer, -3)   AS compensation_interval,
+         false
 )
-INSERT INTO vacancy(vacancy_id, created, employer_id, vacancy_title, area_id, specialization_id, industry_id, vacancy_description, compensation_from, compensation_to, compensation_gross)
+INSERT INTO vacancy(vacancy_id, created, employer_id, vacancy_title, area_id, specialization_id, industry_id,
+                    vacancy_description, compensation_from, compensation_to, compensation_gross)
 SELECT vacancy_id,
        created,
        employer_id,
@@ -44,54 +45,52 @@ SELECT vacancy_id,
        compensation_from,
        compensation_from + compensation_interval,
        compensation_gross
-FROM test_data;
+FROM vacancy_test_data;
 
-WITH test_data( resume_id,
+WITH resume_test_data( resume_id,
                 created,
                 applicant_id,
                 area_id,
                 work_experience,
                 specialization_id,
-                position_name,
+                desired_position,
                 compensation,
                 education_id) AS (
-    SELECT generate_series(1, 100000)                                   AS resume_id,
-           now() - ('1 day'::interval * (random()*180::integer))         AS created,
-           random() * 50000::integer                                    AS applicant_id,
-           1 + random() * 84::integer                                   AS area_id,
-           random() > 0.2                                               AS work_experience,
-           1 + random() * 49::integer                                   AS specialization_id,
-           md5(random()::text)                                          AS position_name,
-           round((random() * (80000 - 15000) + 15000)::integer, -3)    AS compensation,
-           1 + random() * 8::integer                                    AS education_id
+  SELECT generate_series(1, 100000)                               AS resume_id,
+         now() - ('1 day'::interval * (random()*180::integer))    AS created,
+         random() * 50000::integer                                AS applicant_id,
+         1 + random() * 84::integer                               AS area_id,
+         random() > 0.2                                           AS work_experience,
+         1 + random() * 49::integer                               AS specialization_id,
+         md5(random()::text)                                      AS desired_position,
+         round((random() * (80000 - 15000) + 15000)::integer, -3) AS compensation,
+         1 + random() * 8::integer                                AS education_id
 )
-INSERT INTO resume (resume_id, created, applicant_id, area_id, work_experience, specialization_id, position_name, compensation, education_id)
+INSERT INTO resume (resume_id, created, applicant_id, area_id, work_experience, specialization_id, desired_position,
+                    compensation, education_id)
 SELECT  resume_id,
         created,
         applicant_id,
         area_id,
         work_experience,
         specialization_id,
-        position_name,
+        desired_position,
         compensation,
         education_id
-FROM test_data;
+FROM resume_test_data;
 
-
-WITH test_data( vacancy_id,
-                resume_id,
-                created
-                ) AS (
-SELECT v.vacancy_id,
-       resume_id,
-       CASE WHEN (r.created > v.created) THEN r.created::date + (random()*(now()::date - r.created))::integer
-       ELSE v.created::date + (random()*(now()::date - v.created))::integer
-       END AS created
+WITH response_test_data( vacancy_id, resume_id, created) AS (
+  SELECT v.vacancy_id,
+         resume_id,
+         CASE WHEN (r.created > v.created)
+              THEN r.created::date + (random()*(now()::date - r.created))::integer
+              ELSE v.created::date + (random()*(now()::date - v.created))::integer
+         END AS created
 FROM vacancy v
-JOIN resume r ON (  v.area_id = r.area_id
-                AND v.specialization_id = r.specialization_id
-                AND r.compensation > v.compensation_from
-                AND r.compensation < v.compensation_to))
+INNER JOIN resume r ON (v.area_id = r.area_id
+                        AND v.specialization_id = r.specialization_id
+                        AND r.compensation > v.compensation_from
+                        AND r.compensation < v.compensation_to))
 INSERT INTO response(created, resume_id, vacancy_id)
 SELECT created, resume_id, vacancy_id
-FROM test_data;
+FROM response_test_data;
--- task03.sql
+++ task03.sql
@@ -1,6 +1,11 @@
+-- Получение средних значений по регионам (area_id) следующих величин:
+-- compensation_from, compensation_to, среднее_арифметическое_from_и_to,
+-- округление до второго знака, сортировка по полю area_id
+
 SELECT area_id,
-       avg(compensation_from) AS compensation_from,
-       avg(compensation_to) AS compensation_to,
-       avg(compensation_to - compensation_from) AS compensation_avg
+       ROUND(AVG(compensation_from),2) AS avg_compensation_from,
+       ROUND(AVG(compensation_to),2) AS avg_compensation_to,
+       ROUND(AVG((compensation_to + compensation_from)/2),2) AS avg_compensation
 FROM vacancy v
-GROUP BY area_id;
+GROUP BY area_id
+ORDER BY area_id;
